f9f92cf649e3e90acc78572d04aa46f23520e748
EhrenW1 <ehren.wargo@patelhs.org>
2021-02-03 18:38:56 +0000

    I made a scanner that get the name of the users school and what grade their in.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What is the name of your school?");
+    String name = scan.next();
+    System.out.println("What grade are you in?");
+    int number = scan.nextInt();
   }
 }
