f7f823cdfa623c6c5f07205eb0eeb62516bbdb10
EhrenW1 <ehren.wargo@patelhs.org>
2021-02-05 17:25:17 +0000

    I added my two methods which tell you what grade you will be in in one year.

--- Main.java
+++ Main.java
@@ -9,9 +9,25 @@ class Main {
     String school1 = scan.next();
     System.out.println("What grade are you in?");
     int gradeNumber = scan.nextInt();
-
+ int oneYear = 1;
     System.out.println("You go to "+ school1);
     System.out.println("Your current grade is "+ gradeNumber);
+
+    int  gradeInoneYear = getGradeinOneyear( gradeNumber, oneYear);
+    System.out.println(finalStatment(gradeInoneYear));
+  }
+   
+ 
+  static int getGradeinOneyear(int gradeNumber, int oneYear)
+  {
+    int gradeInoneYear = gradeNumber + oneYear;
+    return gradeInoneYear;
+  }
+
+  static String finalStatment(int gradeInoneYear)
+  {
+    String statment = "According to your current grade, in one year the grade you will be in is, " + gradeInoneYear;
+    return statment;
   }
 
 }
